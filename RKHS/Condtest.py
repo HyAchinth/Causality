3116b7519f87a3d7c99e69510ce9bc8256db6671
Achinthya <achinth.s@gmail.com>
2021-06-08 21:52:52 +0530

    Comparision plot vs prob.py added

--- RKHS/Condtest.py
+++ RKHS/Condtest.py
@@ -10,19 +10,6 @@ import matplotlib.pyplot as plt
 from RKHSmod.rkhs import RKHS
 from Probability.Prob import ProbSpace
 
-B = []
-
-def calcB(x):
-    global B
-    B = 0
-    return 
-
-def E(y,x):
-    calcB(x)
-    #result = F(y)
-    
-    #return
-
 def K(x1,x2=0,kparms=[]):
     sigma = kparms[0]
     return ((1/(sigma * math.sqrt(2*math.pi))) * math.exp( -1 * (x1-x2)**2 / (2 * sigma**2) ))
@@ -31,8 +18,8 @@ def F(x,X,kparms=[]):
     sum = 0.0
     for i in X:
         val = F(i,x,kparms)
-        sum += val*B[i]
-    return sum
+        sum += val
+    return sum/len(X)
 
 # Ideal curve for the dataset ( Y = X^2)
 def square(x):
@@ -49,24 +36,24 @@ def square(x):
     
 #     r = RKHS(Y,k = K, f = F, kparms=[2])
 
-#     testPoints = []
-#     testMin = -5
-#     testMax = 5
-#     tp = testMin
-#     numTP = 200
-#     interval = (testMax - testMin) / numTP
-#     sq = []
-#     probpy = []
-#     cond = []
+    # testPoints = []
+    # testMin = -5
+    # testMax = 5
+    # tp = testMin
+    # numTP = 200
+    # interval = (testMax - testMin) / numTP
+    # sq = []
+    # probpy = []
+    # cond = []
     
 #     # Generate a uniform range of test points, expected curve, prob.py curve
-#     for i in range(numTP + 1):
-#         testPoints.append(tp)
-#         y = square(tp)
-#         sq.append(y)
-#         p = ps.distr('Y', [('X', tp)]).E()
-#         probpy.append(p)        
-#         tp += interval
+    # for i in range(numTP + 1):
+    #     testPoints.append(tp)
+    #     y = square(tp)
+    #     sq.append(y)
+    #     p = ps.distr('Y', [('X', tp)]).E()
+    #     probpy.append(p)        
+    #     tp += interval
 
 #     #Generate the conditional distribution using RKHS
 #     # for i in range(len(testPoints)):
@@ -77,27 +64,27 @@ def square(x):
 
 
 #     #plot graph
-#     plt.plot(testPoints,sq, label = 'Ideal')
-#     plt.plot(testPoints,probpy, label = 'ProbSpace')
-#     plt.legend()
-#     plt.show()
+    # plt.plot(testPoints,sq, label = 'Ideal')
+    # plt.plot(testPoints,probpy, label = 'ProbSpace')
+    # plt.legend()
+    # plt.show()
     
-def evalyx(y,x,r1,r2):
-    sum1 = 0
+def evalyx(y,x,r1,r2): #function to calculated E(y|x)
+    #sum1 contains sum(f(x)) and sum2 contains sum(f(y,x))  
+    sum1 = 0    
     sum2 = 0
-    sum3 = 0
     size = len(r1.X)
     for i in range(size):
         t1 = r1.K(x,r1.X[i])
         sum1 += t1
-        t2 = r2.K(y,r2.X[i])
-        sum2 += t2
-        sum3 += t1*t2
-        #print("t1=",t1,"t2=",t2)
+
+        t2 = r2.K(y,r2.X[i])        
+        sum2 += t1*t2
+        #print("x=",x,"y=",y,"X=",r1.X[i],"Y=",r2.X[i])
     
-    print("Sum-P(y,x) =",sum3)
-    print("Sum-P(x) =",sum1)
-    return sum3/sum1
+    # print("Sum-P(y,x) =",sum2)
+    # print("Sum-P(x) =",sum1)
+    return sum2/sum1
         
 
 if __name__ == '__main__':
@@ -107,14 +94,43 @@ if __name__ == '__main__':
     X = data['X']
     Y = data['Y']
 
+    ps = ProbSpace(data)
+
     size = len(X)
     sigma = 1 / math.log(size, 4)
 
     r1 = RKHS(X,kparms=[sigma])
     r2 = RKHS(Y,kparms=[sigma])
 
-    x = 0.5
-    y = 0.1
+    x = 0.7
+    y = 0.04
+
+    print("P(y=",y,"|x=",x,") =",evalyx(y,x,r1,r2))
+        
+    testPoints = []
+    testMin = -2
+    testMax = 2
+    tp = testMin
+    numTP = 200
+    interval = (testMax - testMin) / numTP
+    sq = []
+    probpy = []
+    cond = []
+
+    for i in range(numTP + 1):
+        testPoints.append(tp)
+        
+        p = ps.P(('Y', tp), [('X', x)])
+        probpy.append(p)
+        e = evalyx(tp,x,r1,r2)
+        cond.append(e)       
+        tp += interval
     
-    print("P(y=",y,"|x=",x,") =",evalyx(4,2,r1,r2))
+    plt.plot(testPoints,cond, label = 'RKHS y|x='+str(x))
+    plt.plot(testPoints,probpy, label = 'ProbSpace y|x='+str(x))    
+    plt.legend()
+    plt.show()
+
+
+    print("P(y=",y,"|x=",x,") =",evalyx(y,x,r1,r2))
     
