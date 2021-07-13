34bcd6a314b539f2ce84fdb0234bd7dc886d2b7c
Achinthya <achinth.s@gmail.com>
2021-07-13 18:40:11 +0530

    Probpy comparision and R2 error

--- RKHS/filterRKHS.py
+++ RKHS/filterRKHS.py
@@ -9,8 +9,14 @@ from numpy import *
 from RKHSmod.rkhs import RKHS
 from Condtest2 import *
 
-def tanh(x):
-    return math.tanh(x)
+def ref(x,choice):
+    if choice == 1:
+        return math.tanh(x) + math.sin(conditional)
+    else:
+        return math.sin(x) + math.tanh(conditional)
+
+
+    
 
 
 path = '../models/Cprobdata2.csv'
@@ -22,14 +28,27 @@ P = ProbSpace(data)
 #conditional X value
 conditional = 0
 
-FilterData, parentProb, finalQuery = P.filter([('X',conditional)])
+#choice X=1(tanh) , Y=0(sinh) (choose 1 to condition on X, 0 for Y)
+choice = 1
 
+if choice == 1:
+    FilterData, parentProb, finalQuery = P.filter([('X',conditional)])
+else:
+    FilterData, parentProb, finalQuery = P.filter([('Y',conditional)])
+
+X = FilterData['X']
 Y = FilterData['Y']
 Z = FilterData['Z']
 
+filterlen = len(Z)
+
 s = 0.2 #sigma
 
-r1 = RKHS(Y,kparms=[s])
+if choice == 0:
+    r1 = RKHS(X,kparms=[s])
+else:
+    r1 = RKHS(Y,kparms=[s])
+
 r2 = RKHS(Z,kparms=[s])
 
 testPoints = []
@@ -40,8 +59,14 @@ numTP = 200
 interval = (testMax - testMin) / numTP
 
 Rdev = []
+Pdev = []
 cond = []
 ideal = []
+Probpy = []
+RSqerr = []
+
+PSqerr = []
+Sqmean = []
 
 for i in range(numTP + 1):
     testPoints.append(tp)   
@@ -53,18 +78,38 @@ for i in range(numTP + 1):
         cond.append(0)
     else:
         cond.append(cond[i-1])
-    r = tanh(tp)
+    if choice ==0:
+        p = P.distr('Z', [('X', tp), ('Y', conditional)]).E()
+    else:
+        p = P.distr('Z', [('Y', tp), ('X', conditional)]).E()
+    Probpy.append(p)        
+    r = ref(tp,choice)
     ideal.append(r)            
     err = abs(r-ans)
-    Rdev.append(err)           
-    #print(tp,ans,r,err)
+    Rdev.append(err)
+    err2 = abs(r-p)
+    Pdev.append(err2)   
     tp += interval
 
-print("Average Error:",sum(Rdev)/len(Rdev),"Max error:",max(Rdev))
+ymean = sum(ideal)/len(ideal)
 
-labelstr = 'ProbSpace Z|Y,X='+str(conditional)
+for i in range(numTP + 1):
+    RSqerr.append(Rdev[i]**2)
+    PSqerr.append(Pdev[i]**2)
+    Sqmean.append((ideal[i]-ymean)**2)
 
-plt.plot(testPoints,ideal, label = 'Ideal Curve')
+R2rkhs = 1 - sum(RSqerr)/sum(Sqmean)
+R2probpy = 1 - sum(PSqerr)/sum(Sqmean)
+
+
+print("Filtered Datasize:",filterlen)
+print("Filter-RKHS\tAverage Error:",sum(Rdev)/len(Rdev),"\tR2 error:",R2rkhs)
+print("ProbSpace \tAverage Error:",sum(Pdev)/len(Pdev),"\tR2 error:",R2probpy)
+
+labelstr = 'Filter-RKHS Z|X,Y='+str(conditional)
+
+plt.plot(testPoints,ideal, label = 'Ideal Curve',color='#000000')
+plt.plot(testPoints,Probpy, label = 'ProbSPace')
 plt.plot(testPoints,cond, label = labelstr)    
 plt.legend()
 plt.show()
